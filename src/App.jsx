d9e48724e48180818a5c69ccc2c076535435b937
ashifmahmud <mdamahmud2000@gmail.com>
2025-03-16 19:14:14 +0600

    feat: topbar created

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./Components/Layout/Navbar";
+import Topbar from "./Components/Layout/Topbar";
+
+function App() {
+  return (
+    <>
+      <Topbar />
+      <Navbar />
+    </>
+  );
+}
+
+export default App;
