3c79c81f9308824532dfb8f03e6331af30d76d5c
ashifmahmud <mdamahmud2000@gmail.com>
2025-03-17 21:57:19 +0600

    feat: yellow bar added

--- src/Components/Layout/Topbar.jsx
+++ src/Components/Layout/Topbar.jsx
@@ -5,24 +5,29 @@ import { MdAddIcCall, MdMessage } from "react-icons/md";
 
 const Topbar = () => {
   return (
-    <div className="bg-topbar py-4 font-poppins">
-      <div className="max-w-max-width mx-auto flex justify-between">
-        <div className="flex items-center justify-between w-[40%]">
-          <div className="text-white flex items-center gap-x-2">
-            <MdMessage /> mail@company.com
+    <div>
+      <div className="bg-topbar py-4 font-poppins">
+        <div className="max-w-max-width mx-auto flex justify-between">
+          <div className="flex items-center justify-between w-[40%]">
+            <div className="text-white flex items-center gap-x-2">
+              <MdMessage /> mail@company.com
+            </div>
+            <div className="w-[2px] h-6 bg-[#5C6A92]"></div>
+            <div className="text-white flex items-center gap-x-2">
+              <MdAddIcCall />
+              +896 120 5889 (Toll free)
+            </div>
           </div>
-          <div className="w-[2px] h-6 bg-[#5C6A92]"></div>
-          <div className="text-white flex items-center gap-x-2">
-            <MdAddIcCall />
-            +896 120 5889 (Toll free)
+          <div className="flex text-white items-center gap-x-10 text-xl">
+            <FaFacebookF />
+            <FaTwitter />
+            <FaLinkedinIn />
+            <FaInstagram />
           </div>
         </div>
-        <div className="flex text-white items-center gap-x-10 text-xl">
-          <FaFacebookF />
-          <FaTwitter />
-          <FaLinkedinIn />
-          <FaInstagram />
-        </div>
+      </div>
+      <div className="text-white bg-black pt-1 pb-1">
+        <div className="bg-[#FFB800] h-[2px] pt-1 ml-1 mr-1"></div>
       </div>
     </div>
   );
