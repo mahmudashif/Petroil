0ca11c711a4549a34c17d25aca79e043c3c14076
ashifmahmud <mdamahmud2000@gmail.com>
2025-03-17 22:22:20 +0600

    feat: nav item added

--- src/Components/Layout/Navbar.jsx
+++ src/Components/Layout/Navbar.jsx
@@ -1,14 +1,37 @@
 import React from "react";
 import logo from "/public/Logo.svg";
 
+const navItems = [
+  { item: "Home", path: "/" },
+  { item: "About", path: "/about" },
+  { item: "Services", path: "/services" },
+  { item: "Portfolio", path: "/portfolio" },
+  { item: "Contact", path: "/contact" },
+];
+
 const Navbar = () => {
   return (
     <div className="bg-navbarColor">
-      <div className="max-w-max-width mx-auto py-2">
+      <div className="max-w-max-width mx-auto py-2 flex justify-between items-center">
         <div>
-          <img src={logo} alt="" />
+          <img src={logo} alt="Logo" className="h-10" />
         </div>
-        <div></div>
+        <nav>
+          <ul className="flex space-x-6">
+            {navItems.map((nav, index) => (
+              <li key={index}>
+                <a href={nav.path} className="text-white hover:text-gray-300">
+                  {nav.item}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <div>
+          <button className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600">
+            Get Started
+          </button>
+        </div>
       </div>
     </div>
   );
