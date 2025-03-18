f5a1aeb29d31c94f952de0fc34484520e2a31a9f
ashifmahmud <mdamahmud2000@gmail.com>
2025-03-18 22:23:57 +0600

    fix: button design fix

--- src/Components/Layout/Navbar.jsx
+++ src/Components/Layout/Navbar.jsx
@@ -28,8 +28,8 @@ const Navbar = () => {
           </ul>
         </nav>
         <div>
-          <button className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600">
-            Get Started
+          <button className="border-[2px] px-8 py-3 text-white font-poppins font-bold">
+            Contact
           </button>
         </div>
       </div>
