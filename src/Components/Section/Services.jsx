ec5b7bb59307aac5062fb939ad40441c5ff78a5c
ashifmahmud <mdamahmud2000@gmail.com>
2025-03-20 21:33:55 +0600

    feat: services part added

--- src/Components/Section/Services.jsx
+++ src/Components/Section/Services.jsx
@@ -3,6 +3,9 @@ import servicesImg1 from "/public/servicesImg1.svg";
 import servicesImg2 from "/public/servicesImg2.svg";
 import servicesImg3 from "/public/servicesImg3.svg";
 import servicesImg4 from "/public/servicesImg4.svg";
+import servicesImg5 from "/public/servicesImg5.svg";
+import servicesImg6 from "/public/servicesImg6.svg";
+import servicesImg7 from "/public/servicesImg7.svg";
 
 const Services = () => {
   return (
@@ -13,13 +16,24 @@ const Services = () => {
         <img src={servicesImg3} alt="" />
         <img src={servicesImg4} alt="" />
       </div>
-      <div>
-        <div>
+      <div className="flex justify-between mt-10">
+        <div className="w-[50%]">
           <h3>our services</h3>
           <p>
             It is a long established fact that a reader will be distracted by
             the readable content of a page when looking at its layout.
           </p>
+        </div>
+        <div className="w-[50%]">
+          <img src={servicesImg5} alt="" />
+        </div>
+      </div>
+      <div className="flex">
+        <div className="w-[50%]">
+          <img src={servicesImg6} alt="" />
+        </div>
+        <div className="w-[50%]">
+          <img src={servicesImg7} alt="" />
         </div>
       </div>
     </div>
