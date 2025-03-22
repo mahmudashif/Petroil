d851dc7fe74f8ddc537577750576ec6044bd5558
ashifmahmud <mdamahmud2000@gmail.com>
2025-03-22 23:31:53 +0600

    feat: footer logo

--- src/Components/Layout/Footer.jsx
+++ src/Components/Layout/Footer.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import logo from "/public/footerLogo.svg";
 
 const Footer = () => {
   return (
     <div className="bg-footerColor py-[149px]">
-      <div className="text-white max-w-max-width mx-auto">footer</div>
+      <div className="text-white max-w-max-width mx-auto">
+        {/* ---------- footer first coloum ---------- */}
+        <div>
+          <div>
+            <img src={logo} alt="" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
