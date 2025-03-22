da7cb901f544ebd2520ecd26cb0164498b547ebf
ashifmahmud <mdamahmud2000@gmail.com>
2025-03-22 23:40:53 +0600

    feat: first part done

--- src/Components/Layout/Footer.jsx
+++ src/Components/Layout/Footer.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { FaMapLocationDot } from "react-icons/fa6";
+import { IoMailUnreadOutline } from "react-icons/io5";
+import { MdAddIcCall } from "react-icons/md";
 import logo from "/public/footerLogo.svg";
 
 const Footer = () => {
@@ -9,6 +12,20 @@ const Footer = () => {
         <div>
           <div>
             <img src={logo} alt="" />
+          </div>
+          <div className="mt-8 space-y-4">
+            <p className="flex items-center gap-x-3">
+              <IoMailUnreadOutline />
+              mail@yourcompany.com
+            </p>
+            <p className="flex items-center gap-x-3">
+              <MdAddIcCall />
+              +123456789
+            </p>
+            <p className="flex items-center gap-x-3">
+              <FaMapLocationDot />
+              101 baker street, NYC
+            </p>
           </div>
         </div>
       </div>
